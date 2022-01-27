f4d04077fc63cc0d898d39c7a7f7e75f9bb07222
LuK1337 <priv.luk@gmail.com>
2022-01-27 12:10:21 +0100

    Embrace MTG this time around
    Change-Id: I703824505f3de003159fdf0eba10dfc8e6827c78

--- product.mk
+++ product.mk
@@ -4,5 +4,5 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 # Google Apps
 ifeq ($(WITH_GMS), true)
-$(call inherit-product, vendor/gapps/common/common-vendor.mk)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
 endif
