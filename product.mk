29a78158a420fab9ebc6526b1da8ede0aa16a3f9
Tommaso Fonda <fondatommaso2@gmail.com>
2026-02-20 14:17:43 +0100

    Add PIHooks props
    Change-Id: Ia2f56cdec89acff05691493e1ba8556e23ac8aaf

--- product.mk
+++ product.mk
@@ -37,3 +37,19 @@ PRODUCT_SYSTEM_PROPERTIES += \
 ifeq ($(WITH_GMS), true)
 $(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 endif
+
+# PIF values
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.pihooks_MANUFACTURER?=Google \
+    persist.sys.pihooks_BRAND?=google \
+    persist.sys.pihooks_PRODUCT?=raven_beta \
+    persist.sys.pihooks_DEVICE?=raven \
+    persist.sys.pihooks_ID?=CP21.260116.011.A1 \
+    persist.sys.pihooks_RELEASE?=16 \
+    persist.sys.pihooks_SECURITY_PATCH?=2025-12-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PihooksGmsFp="google/raven_beta/raven:CinnamonBun/CP21.260116.011.A1/14876943:user/release-keys" \
+    PihooksGmsModel="Pixel 6 Pro"
