efe79a966ea899318cf81290424d5d4e03215270
Tommaso Fonda <fondatommaso2@gmail.com>
2026-03-27 13:21:09 +0100

    fixup! update pihooks values

--- product.mk
+++ product.mk
@@ -44,12 +44,12 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_BRAND?=google \
     persist.sys.pihooks_PRODUCT?=blazer_beta \
     persist.sys.pihooks_DEVICE?=blazer \
-    persist.sys.pihooks_ID?=CP21.260206.011 \
-    persist.sys.pihooks_RELEASE?=16 \
-    persist.sys.pihooks_SECURITY_PATCH?=2026-02-05 \
+    persist.sys.pihooks_ID?=CP21.260306.017 \
+    persist.sys.pihooks_RELEASE?=17 \
+    persist.sys.pihooks_SECURITY_PATCH?=2026-03-05 \
     persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
     persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/blazer_beta/blazer:CinnamonBun/CP21.260206.011/14911669:user/release-keys" \
+    PihooksGmsFp="google/blazer_beta/blazer:17/CP21.260306.017/15063635:user/release-keys" \
     PihooksGmsModel="Pixel 10 Pro"
